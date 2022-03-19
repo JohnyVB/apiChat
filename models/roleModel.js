d167e773fbc5acb20b36a60fb464e1c78bf2d4bd
Johny Villegas <johnyvgoog@gmail.com>
2022-03-19 17:53:53 -0500

    Archivos base

--- models/roleModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const RoleSchema = new mongoose.Schema({
-    role: {
-        type: String
-    }
-});
-
-module.exports = mongoose.model('Role', RoleSchema);
